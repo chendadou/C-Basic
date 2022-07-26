cc7345fc19cca1c91f3416cf2c73a8407b6eaa78
chendadou <463363269@qq.com>
2022-07-26 13:16:15 +0800

    modify code/day06 and note06

--- code/day06/main.c
+++ code/day06/main.c
@@ -79,8 +79,38 @@ void test_f()
   printf("total = %d, i = %d\n", total, i);
 }
 
+/**
+ * 7. 输入一个整形数，判断其是否为对称数；
+ * 比如，12321、67876 是对称数，而 1234 不是对称数；
+ * 若是对称数则输出 yes，反之输出 no。
+**/
+void test_g()
+{
+  int a, b, a_copy;
+  printf("请输入整数：");
+  scanf("%d", &a);
+  a_copy = a;
+  b = 0;
+  while(a){
+    b = b * 10 + a % 10; 
+    a = a / 10;
+  }
+  if(b == a_copy)
+    printf("yes.\n");
+  else
+    printf("no.\n");
+}
+
 int main()
 {
+  printf("1. if 和 else\n");
+  printf("2. 从 1 加到 100（while 循环）\n");
+  printf("3. 从 1 加到 100（for 循环）\n");
+  printf("4. 从 1 到 100 之间的奇数之和（continue 和 for）\n");
+  printf("5. 从 1 到 100 之间的奇数之和（continue 和 while）\n");
+  printf("6. 从 1 加到 100，当 total 大于 1000 时结束循环\n");
+  printf("7. 输入一个整形数，判断其是否为对称数；\n");
+  printf("请输入序号，运行相应例子：");
   int num;
   scanf("%d", &num);
   switch(num){
@@ -102,8 +132,11 @@ int main()
     case 6:
       test_f();
       break;
+    case 7:
+      test_g();
+      break;
     default:
-      printf("...");
+      printf("序号不存在。");
       break;
   }
   return 0;
